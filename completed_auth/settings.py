7fce8b49976c47f1081021f5745234754384c658
Sam <sam.eskandar00@hotmail.com>
2020-08-18 00:14:57 -0400

    remove debug mode

--- completed_auth/settings.py
+++ completed_auth/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$m3dma$fil%ujgr$_hir9y5@i4l3at#z!mhjw#6j%_y(&q0_#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
